2142012e4e339f16906eba83cdf1f49f805d204d
hiro <hiro.libe.0930@gmail.com>
2024-04-16 23:05:42 +0900

    addition

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
+from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.shortcuts import render
-
+from apiclient.discovery import build
+from datetime import datetime, timedelta, date
+from django.conf import settings
+from .forms import KeywordForm
+import pandas as pd
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-se0e-t*z4*a_&#c+*v0(fctaq$w6j1%c)46wa&fy*nl@nz=&w=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://django-render-fx8l.onrender.com"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     'app'
 ]
 
