93b9ca6aa052d965d3c500761c4bba18033e89da
hiro <hiro.libe.0930@gmail.com>
2024-04-22 15:30:58 +0900

    add update.py

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,5 @@ from app import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("update/", views.UpdateView.as_view(), name="update"),
     path("keyword/", views.KeywordView.as_view(), name="keyword"),
 ]
