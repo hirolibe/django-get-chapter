cb6be578dc1c123d48094203eb16cbe4b5169827
hiro <hiro.libe.0930@gmail.com>
2024-04-22 17:56:10 +0900

    revised

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,7 +17,7 @@ from decouple import config
 from dj_database_url import parse as dburl
 
 # ALLOWED_HOSTS = ["django-render-fx8l.onrender.com"]
-ALLOWED_HOSTS = ["＊"]
+ALLOWED_HOSTS = ["*"]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
