1f3eb7385a8ca9c15509dc308e1fa53c4b1ba076
hiro <hiro.libe.0930@gmail.com>
2025-05-24 08:35:08 +0900

    ルーティングの修正

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,8 @@ from app import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("<int:page>", views.IndexView.as_view(), name="index"),
     path("update/", views.UpdateView.as_view(), name="update"),
     path("chapter/", views.ChapterView.as_view(), name="chapter"),
     path("chapter/<int:page>", views.ChapterView.as_view(), name="chapter"),
+    path("<int:page>", views.IndexView.as_view(), name="index"),
 ]
