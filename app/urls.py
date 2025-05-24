f530e08bfbb40ce2fb25628fa3f880978616b8e2
hiro <hiro.libe.0930@gmail.com>
2025-05-24 09:14:27 +0900

    ルーティングの検証

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    # path("", views.IndexView.as_view(), name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     path("update/", views.UpdateView.as_view(), name="update"),
     path("chapter/", views.ChapterView.as_view(), name="chapter"),
-    path("chapter/<int:page>", views.ChapterView.as_view(), name="chapter"),
-    # path("<int:page>", views.IndexView.as_view(), name="index"),
+    path("chapter/<int:page>/", views.ChapterView.as_view(), name="chapter_page"),
+    path("<int:page>/", views.IndexView.as_view(), name="index_page"),
 ]
